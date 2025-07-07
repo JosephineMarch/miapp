545baa38d0096887a1af1bdcc4003c5e6f5c56ab
JosephineMarch <marymariablog@gmail.com>
2025-07-06 20:58:24 -0500

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // --- MODO DE DESARROLLO ---
 // Cambia a 'true' para trabajar sin login (usando localStorage).
 // Cambia a 'false' para activar Firebase y el login de Google para producción.
-const DEVELOPMENT_MODE = true;
+const DEVELOPMENT_MODE = false;
 
 // --- 1. IMPORTAR FUNCIONES DE FIREBASE ---
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
